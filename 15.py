f67caec02af344b29a54cb73ca952e26c7ac3202
Tony <tonymajestro@gmail.com>
2022-12-16 17:11:28 -0800

    Update day 15

--- 15.py
+++ 15.py
@@ -2,11 +2,10 @@
 
 import sys
 from collections import namedtuple
-import pdb
+from intervaltree import IntervalTree
 
-ROW_TO_SEARCH = 2000000
+#ROW_TO_SEARCH = 2000000
 BOUNDS = 4000000
-#ROW_TO_SEARCH = 10
 #BOUNDS = 20
 
 def manhattanDistance(sensor, beacon):
@@ -25,36 +24,37 @@ def findNoBeacons(sensorsAndBeacons, rowToSearch):
 
     return noBeacons
 
-def findNoBeaconsWithBounds(sensorsAndBeacons, rowToSearch, bounds):
-    noBeacons = set()
+def findNoBeaconsWithBounds(sensorsAndBeacons, rowToSearch):
+    intervals = IntervalTree()
     for sensor, closestBeacon in sensorsAndBeacons:
         distance = manhattanDistance(sensor, closestBeacon)
         if sensor.y - distance <= rowToSearch <= sensor.y + distance:
-            startX = min(0, sensor.x - (distance - abs(sensor.y - rowToSearch)))
-            stopX = max(bounds + 1, sensor.x + (distance - abs(sensor.y - rowToSearch)))
-            for i in range(startX, stopX + 1):
-                noBeacons.add(i)
+            startX = max(0, sensor.x - (distance - abs(sensor.y - rowToSearch)))
+            stopX = min(BOUNDS, sensor.x + (distance - abs(sensor.y - rowToSearch)))
+            intervals[startX : stopX + 1] = (startX, stopX + 1)
+            #intervals.merge_overlaps()
 
-    return noBeacons
+    return intervals
 
 def solve1(sensorsAndBeacons, rowToSearch):
     return findNoBeacons(sensorsAndBeacons, rowToSearch)
 
 def solve2(sensorsAndBeacons):
-    #pdb.set_trace()
     beacons = [beacon for sensor, beacon in sensorsAndBeacons]
     for row in range(BOUNDS + 1):
-        noBeacons = findNoBeaconsWithBounds(sensorsAndBeacons, row, BOUNDS)
-        noBeacons.update(beacon.x for beacon in beacons if beacon.y == row)
+        intervals = findNoBeaconsWithBounds(sensorsAndBeacons, row)
+        for beacon in beacons:
+            if beacon.y == row and 0 <= beacon.x <= BOUNDS:
+                intervals[beacon.x : beacon.x + 1] = (beacon.x, beacon.x + 1)
 
-        print(row)
-        if len(noBeacons) == BOUNDS + 1:
-            continue
-
-        for i in range(BOUNDS + 1):
-            if i not in noBeacons:
-                #pdb.set_trace()
-                return (i * 4000000) + row
+        if row % 10000 == 0:
+            print(row)
+        intervals.merge_overlaps()
+        intervalData = sorted([i.data for i in intervals])
+        for i in range(len(intervalData) - 1):
+            currInterval, nextInterval = intervalData[i], intervalData[i + 1]
+            if currInterval[1] != nextInterval[0]:
+                return ((currInterval[1]) * 4000000) + row
         
 if __name__ == '__main__':
     if len(sys.argv) == 2:
@@ -70,6 +70,6 @@ if __name__ == '__main__':
         sensorX, sensorY, beaconX, beaconY = int(split[2][2:-1]), int(split[3][2:-1]), int(split[8][2:-1]), int(split[9][2:])
         sensorsAndBeacons.append((Point(sensorX, sensorY), Point(beaconX, beaconY)))
     
-    print(len(solve1(sensorsAndBeacons, ROW_TO_SEARCH)))
+    #print(len(solve1(sensorsAndBeacons, ROW_TO_SEARCH)))
     print(solve2(sensorsAndBeacons))
 
